cd2524248151396233629a09b4034d21cca7d909
GPersinger <70274068+GPersinger@users.noreply.github.com>
2020-10-13 11:42:14 -0500

    buidl of 4 levels with scene updates

---
+++ Assets/ChangeLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeLevel : MonoBehaviour
+{
+    [Tooltip("The Name of the scene you want to go to.")]
+    public string destination;
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            if(destination == "") destination = "Main Menu";
+            UnityEngine.SceneManagement.SceneManager.LoadScene(destination);
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool phoneIsConnected = false;
+    [HideInInspector]
+    public Vector3 dir;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(phoneIsConnected){
+            dir.x = Input.acceleration.x;
+            dir.z = Input.acceleration.y;
+
+        }else{
+            dir.x = Input.GetAxis("Horizontal");
+            dir.z = Input.GetAxis("Vertical");
+        }
+        rb.AddForce(dir * 10);
     }
 }
